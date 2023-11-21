aaf3ab092a335ac8197d09660296be47a9438632
rrayhka <khyar075@gmail.com>
2023-11-21 18:10:31 +0700

    "update"

--- kuliah/meet13/orders/order.php
+++ kuliah/meet13/orders/order.php
@@ -63,6 +63,7 @@ if (isset($_GET['order_id'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"></head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
