98d0678c68cb853d473e04f2f379b314307be520
rrayhka <khyar075@gmail.com>
2023-11-21 11:12:06 +0700

    update

--- praktikum/meet8/index.php
+++ praktikum/meet8/index.php
@@ -30,7 +30,6 @@
     <title>Document</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        /* Style dropdown */
         .dropdown {
             position: relative;
             display: inline-block;
--- praktikum/meet8/login.php
+++ praktikum/meet8/login.php
@@ -28,18 +28,28 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form method="post">
-            <label for="username">Masukkan Username</label>
-            <input type="text" name="username" required><br>
-            <label for="password">Masukkan password</label>
-            <input type="password" name="password" required><br>
-            <button type="submit" name="submit">Submit</button>
-        </form> 
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 flex justify-center items-center h-screen">
+    <div class="bg-white p-8 rounded shadow-md">
+        <h1 class="text-2xl font-semibold mb-4 text-center">Login</h1>
+        <form method="post" class="space-y-4">
+            <div>
+                <label for="username" class="block">Username</label>
+                <input type="text" name="username" required class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+            </div>
+            <div>
+                <label for="password" class="block">Password</label>
+                <input type="password" name="password" required class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+            </div>
+            <button type="submit" name="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                Login
+            </button>
+        </form>
+    </div>
+</body>
 </html>
